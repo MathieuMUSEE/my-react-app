e8774bc0f38a2b54f8e324ead4aa1a0beba9a72c
mathieu.muse <mathieu.muse@outlook.fr>
2025-03-24 12:10:11 +0100

    push API component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import UsersList from "./UsersList";
 
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">Hello, World!</h1>
+      <UsersList />
     </div>
   );
 };
---
+++ src/UsersList.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function UsersList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://workout-planner-service:3000/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des utilisateurs :', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Liste des Utilisateurs</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.nom} {user.prenom}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UsersList;
